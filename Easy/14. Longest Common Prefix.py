17679f16cf7ec5fb3a38b7930e536074245da9be
JanEickholt <jan.eickholt50@gmail.com>
2023-11-01 22:04:58 +0100

    Added problem 14

---
+++ Easy/14. Longest Common Prefix.py
@@ -0,0 +1,13 @@
+def longestCommonPrefix(self, strs):
+    length = len(strs[0])
+    i = 0
+    result = ""
+
+    while i < length:
+        for word in strs:
+            if i >= len(word) or word[i] != strs[0][i]:
+                return result
+        result += strs[0][i]
+        i += 1
+
+    return result
