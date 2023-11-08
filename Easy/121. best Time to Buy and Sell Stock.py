82ef8c65192141603ba872b6e11076fa05600545
JanEickholt <eickholtjan@gmail.com>
2023-11-08 21:30:25 +0100

    Added problem 121

---
+++ Easy/121. best Time to Buy and Sell Stock.py
@@ -0,0 +1,10 @@
+def maxProfit(prices):
+    max_profit = 0
+    buy_day = prices[0]
+    for sell_day in prices[1:]:
+        if sell_day > buy_day:
+            max_profit = max(max_profit, sell_day - buy_day)
+        else:
+            buy_day = sell_day
+
+    return max_profit
