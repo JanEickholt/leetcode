74a0f52ef78535b67bb6a0bb2de149dce8a08b2c
JanEickholt <eickholtjan@gmail.com>
2023-11-16 21:43:47 +0100

    Added problem 217

--- Easy/217. Contains Dublicate.py
+++ Easy/217. Contains Dublicate.py
@@ -1,14 +1,2 @@
 def containsDuplicate(nums):
     return len(set(nums)) != len(nums)
-
-
-
-def containsDuplicate(nums):
-    seen = {}
-    for i in nums:
-        if i in seen:
-            return True
-        else:
-            seen[i] = True
-
-    return False
