7c1646fcd4a32450095336d2cb6a3df70608e726
JanEickholt <eickholtjan@gmail.com>
2023-11-24 00:33:52 +0100

    Updated problem 26

--- Easy/26. Remove Dublicates from Sorted Array.py
+++ Easy/26. Remove Dublicates from Sorted Array.py
@@ -2,15 +2,10 @@ def removeDuplicates(nums):
     if not nums:
         return 0
 
-    number = nums[0]
     i = 0
-    while i < len(nums)-1:
-        if number == nums[i+1]:
-            nums.pop(i+1)
-            continue
-        i+=1
-        number = nums[i]
+    for j in range(1, len(nums)):
+        if nums[j] != nums[i]:
+            i += 1
+            nums[i] = nums[j]
 
-    print(nums)
-    return len(nums)
-
+    return i + 1
