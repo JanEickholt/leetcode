75aab2a5134189145f0d6381f53162564039320d
JanEickholt <eickholtjan@gmail.com>
2023-11-27 01:48:27 +0100

    Added problem 125

---
+++ Easy/125. Valid Palindrome.py
@@ -0,0 +1,6 @@
+import re
+def isPalindrome(s):
+    stripped = re.findall(r'([0-9A-Za-z]+)', s)
+    stripped = ''.join(stripped)
+    stripped = stripped.lower()
+    return stripped == stripped[::-1]
