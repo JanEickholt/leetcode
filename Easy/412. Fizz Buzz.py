62d7a11c8f486985bc974673e31bfdbae5c0451f
Jan Eickholt <eickholtjan@gmail.com>
2023-12-26 18:46:48 -0600

    Added problem 412

---
+++ Easy/412. Fizz Buzz.py
@@ -0,0 +1,18 @@
+def fizzBuzz(self, n):
+    res = []
+
+    for i in range(1, n + 1):
+        if i % 15 == 0:
+            res.append('FizzBuzz')
+            continue
+        if i % 3 == 0:
+            res.append('Fizz')
+            continue
+        if i % 5 == 0:
+            res.append('Buzz')
+            continue
+
+        res.append(str(i))
+
+    return res
+
