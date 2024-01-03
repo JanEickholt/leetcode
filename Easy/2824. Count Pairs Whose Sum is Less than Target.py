6c781da86e0a0f409eddaf4ad519be07ecc5a2d9
Jan Eickholt <eickholtjan@gmail.com>
2024-01-03 23:12:41 +0100

    Added problem 2824

---
+++ Easy/2824. Count Pairs Whose Sum is Less than Target.py
@@ -0,0 +1,10 @@
+def countPairs(nums, target):
+    k = 0
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] < target:
+                k += 1
+                j += 1
+            i += 1
+        return k
+
