c77ebe6708752ed0bf3462df80a7f19429881743
JanEickholt <eickholtjan@gmail.com>
2024-01-25 23:30:18 +0100

    Added problem 2591

---
+++ Easy/2591. Distribute Money to Maximum Children.py
@@ -0,0 +1,9 @@
+def distMoney(money, children):
+    if children > money:
+        return -1
+    if money > 8 * children:
+        return children - 1
+    money -= children
+    if children - money // 7 == 1 and money % 7 == 3:
+        return money // 7 - 1
+    return money // 7
