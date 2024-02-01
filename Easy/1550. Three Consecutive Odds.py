9826bd8f6421af77be653ae1489c4066e00e3abd
JanEickholt <eickholtjan@gmail.com>
2024-02-01 21:52:38 +0100

    Added problem 1550

---
+++ Easy/1550. Three Consecutive Odds.py
@@ -0,0 +1,5 @@
+def threeConsecutiveOdds(arr):
+    for i in range(len(arr) - 2):
+        if arr[i] % 2 == 1 and arr[i + 1] % 2 == 1 and arr[i + 2] % 2 == 1:
+            return True
+    return False
