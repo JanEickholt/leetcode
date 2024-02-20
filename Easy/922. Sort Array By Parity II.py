889c5e4ca4a8663bffba530c187a742e299b6a85
JanEickholt <eickholtjan@gmail.com>
2024-02-20 23:06:36 +0100

    Added problem 922

---
+++ Easy/922. Sort Array By Parity II.py
@@ -0,0 +1,14 @@
+def sortArrayByParityII(nums):
+    sorted_nums = [0] * len(nums)
+    even_index = 0
+    odd_index = 1
+
+    for num in nums:
+        if num % 2 == 0:
+            sorted_nums[even_index] = num
+            even_index += 2
+        else:
+            sorted_nums[odd_index] = num
+            odd_index += 2
+
+    return sorted_nums
