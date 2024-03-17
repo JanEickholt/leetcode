8b7a3fae71ca025524ecd237517a3bfb75144d76
JanEickholt <eickholtjan@gmail.com>
2024-03-17 22:43:55 +0100

    Added problem 804

---
+++ Easy/804. Unique Morse Code Words.py
@@ -0,0 +1,34 @@
+def uniqueMorseRepresentations(words):
+    morse_dict = {
+        "a": ".-",
+        "b": "-...",
+        "c": "-.-.",
+        "d": "-..",
+        "e": ".",
+        "f": "..-.",
+        "g": "--.",
+        "h": "....",
+        "i": "..",
+        "j": ".---",
+        "k": "-.-",
+        "l": ".-..",
+        "m": "--",
+        "n": "-.",
+        "o": "---",
+        "p": ".--.",
+        "q": "--.-",
+        "r": ".-.",
+        "s": "...",
+        "t": "-",
+        "u": "..-",
+        "v": "...-",
+        "w": ".--",
+        "x": "-..-",
+        "y": "-.--",
+        "z": "--..",
+    }
+    words = set(words)
+    result = set()
+    for i in words:
+        result.add("".join([morse_dict[j] for j in i]))
+    return len(result)
