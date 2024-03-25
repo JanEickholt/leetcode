b3992ffe06d982a3f6747bdec465c8b2542b81b6
Jan Eickholt <eickholtjan@gmail.com>
2024-03-25 12:51:16 +0100

    Added problem 704

---
+++ Easy/704.binary-search.py
@@ -0,0 +1,12 @@
+def search(nums: List[int], target: int) -> int:
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = int((left + right) // 2)
+        if nums[mid] < target:
+            left = mid + 1
+        elif nums[mid] > target:
+            right = mid - 1
+        else:
+            return mid
+    return -1
